136c3d195e142dd80c94b5437bcd8378980f032d
Getúlio Rafael Ferreira <gferreiraa@gmail.com>
2023-09-19 21:37:43 -0300

    feat(site) layout config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main>
+      <h1 className="px-10 py-10 text-center">Hello World</h1>
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Footer = (props: any) => {
+  return (
+    <>
+      <div className="bg-[#212126] text-white h-[1840px]">
+        <div className="container mx-auto">
+          <h5 className="text-3xl text-center pt-[200px] uppercase">
+            Read to set sail
+          </h5>
+          <p className="text-center pt-5">
+            You've got what it takes to steer this ship to success. Let's
+            discuss how we can clear the way.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
