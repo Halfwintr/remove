ef22df756b0b1c2290d2d30087fc73a4979afe40
Getúlio Rafael Ferreira <gferreiraa@gmail.com>
2023-09-27 08:55:32 -0300

    feat(site): created blog dynamic routing

--- src/app/blogs/[slug]/layout.tsx
+++ src/app/blogs/[slug]/layout.tsx
@@ -1,15 +1,10 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-import { Header } from "../components/Header";
-import { Hero } from "@/components/Hero/Hero";
-import { Team } from "@/components/Team/Team";
-import { Newsletter } from "@/components/Newsletter/Newsletter";
+import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Blogpost } from "@/components/Blogpost/Blogpost";
-import { Support } from "@/components/Suport/Support";
 
 export const metadata: Metadata = {
   title: "Re:move",
@@ -25,15 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>
-          <Hero />
-          <Hero mirror revert />
-          <Support />
-          <Team />
-          <Newsletter />
-          <Blogpost />
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
